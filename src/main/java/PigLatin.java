0582fcc8b09e44570e566fd47fc2b93eaa95f23a
Ben11111111 <ben042554@gmail.com>
2025-01-24 23:10:00 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -29,7 +29,7 @@ public class PigLatin {
     }
 
    public int findFirstVowel(String sWord) {
-  for (int i = 0; i< Sword.length(); i++)
+  for (int i = 0; i< sWord.length(); i++)
     if (sWord.substring(i, i+1).equals("a") || sWord.substring(i, i+1).equals("e") || sWord.substring(i, i+1).equals("i") || sWord.substring(i, i+1).equals("o") || sWord.substring(i, i+1).equals("u")) {
 
       return i;
